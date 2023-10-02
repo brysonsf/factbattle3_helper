64c8e53e7aea92c3967c5f747e9ba2c905f4015f
bryson <br441570@dal.ca>
2023-10-01 23:49:38 -0300

    now removes each pokemon instance-by-instance one-by-one

--- scripts.js
+++ scripts.js
@@ -4,70 +4,71 @@ const NUM_COLUMNS = 16;
 const NUM_ROWS = 5;
 var changeList = [];
 
-// read input and build pokemon data table
-$("#input").on("change", function(e) {
-  var file = e.target.files[0];
-  // input canceled, return
-  if (!file) return;
+document.addEventListener("DOMContentLoaded", function(event) { 
+  // read input and build pokemon data table
+  $("#input").on("change", function(e) {
+    var file = e.target.files[0];
+    // input canceled, return
+    if (!file) return;
 
-  var FR = new FileReader();
-  FR.onload = function(e) {
-    var data = new Uint8Array(e.target.result);
-    var workbook = XLSX.read(data, {
-      type: 'array'
-    });
-    // sheet 1 - pokemon + brains
-    var firstSheet = workbook.Sheets[workbook.SheetNames[0]];
+    var FR = new FileReader();
+    FR.onload = function(e) {
+      var data = new Uint8Array(e.target.result);
+      var workbook = XLSX.read(data, {
+        type: 'array'
+      });
+      // sheet 1 - pokemon + brains
+      var firstSheet = workbook.Sheets[workbook.SheetNames[0]];
 
-    // header: 1 instructs xlsx to create an 'array of arrays'
-    var result = XLSX.utils.sheet_to_json(firstSheet, {
-      header: 1
-    });
-    // console.log(result[1]); this is Sunkern
+      // header: 1 instructs xlsx to create an 'array of arrays'
+      var result = XLSX.utils.sheet_to_json(firstSheet, {
+        header: 1
+      });
+      // console.log(result[1]); this is Sunkern
 
-    // create body
-    const body = document.body;
-    // create table
-    const tbl = createTable(result);
-    tbl.className="table pokeTable";
-    const inputToHide = document.getElementById('input-hider')
-    if(inputToHide){
-    	inputToHide.parentNode.removeChild(inputToHide)
+      // create body
+      const body = document.body;
+      // create table
+      const tbl = createTable(result);
+      const inputToHide = document.getElementById('input-hider')
+      if(inputToHide){
+        inputToHide.parentNode.removeChild(inputToHide)
+      }
+      body.appendChild(tbl);
     }
-    body.appendChild(tbl);
-  }
-  FR.readAsArrayBuffer(file);
-});
-
-const tipButton = document.getElementById('tipButton');
-if(tipButton){
-  tipButton.addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-      document.getElementById('tipButton').innerHTML = "Show Tips on Scientist Quotes";
-    } else {
-      content.style.display = "block";
-      document.getElementById('tipButton').innerHTML = "Hide Tips on Scientiest Quotes";
-    }
-  });  
-}
-
-const dataButton = document.getElementById('dataButton');
-if(dataButton){
-  dataButton.addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-      document.getElementById('dataButton').innerHTML = "Show Scientiest Exact Moves";
-    } else {
-      content.style.display = "block";
-      document.getElementById('dataButton').innerHTML = "Hide Scientist Exact Moves";
-    }
+    FR.readAsArrayBuffer(file);
   });
-}
+  const tipButton = document.getElementById('tipButton');
+  if(tipButton){
+    tipButton.addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+        document.getElementById('tipButton').innerHTML = "Show Tips on Scientist Quotes";
+      } else {
+        content.style.display = "block";
+        document.getElementById('tipButton').innerHTML = "Hide Tips on Scientiest Quotes";
+      }
+    });  
+  }
+
+  const dataButton = document.getElementById('dataButton');
+  if(dataButton){
+    dataButton.addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+        document.getElementById('dataButton').innerHTML = "Show Scientiest Exact Moves";
+      } else {
+        content.style.display = "block";
+        document.getElementById('dataButton').innerHTML = "Hide Scientist Exact Moves";
+      }
+    });
+  }
+
+});
 // pokemon search function
 function findPokemon() {
 	removeButtonShadow('findButton');
@@ -105,10 +106,22 @@ function clearPokemon() {
     pokeSearchString = pokeSearchString.toLowerCase();
     const pokeTableQueryString = '.' + pokeSearchString;
     // search for each instance of mon ? or ask for it in input
+    // just add mon and check if more exist lol
     // scroll to row using autoScroll()
+    let seenPrior = 0;
     if(pokeTableQueryString){
-        changeList.push(pokeSearchString);
-        el.style.display = 'none';
+      changeList.forEach(element => {
+        if(element===pokeSearchString){
+          seenPrior+=1;
+        }
+      });
+      changeList.push(pokeSearchString);
+      const cells = document.getElementsByClassName(pokeSearchString+seenPrior);
+      var cellsArr = Array.prototype.slice.call( cells )
+      cellsArr.forEach(cell => {
+        cell.style.display='none';
+      })
+      //el.style.display = 'none';
     }
   }
   
@@ -138,12 +151,13 @@ function createTable(result) {
       // ignore headers - deal w each pokemon here
       let pkmnData = result[counter] || "";
       let pkmnName = pkmnData[1] || "";
+      let pkmnInstance = pkmnData[2] || "";
       for (let g = 0; g < NUM_COLUMNS; g++) {
         if (pkmnData[g] == null) {
           pkmnData[g] = "NO DATA";
         }
         // user outerHTML to overwrite td
-        row.insertCell(g).outerHTML = "<td class=" + pkmnName.toLowerCase() + ">" + pkmnData[g] + "</td>";
+        row.insertCell(g).outerHTML = "<td class=" + pkmnName.toLowerCase() + pkmnInstance + ">" + pkmnData[g] + "</td>";
         if (pkmnName !== null) {
           pkmnData[1] = pkmnName || pkmnData[1];
         }
@@ -176,6 +190,6 @@ function removeButtonShadow(buttonString){
 }
 // currently only scrolls to the end of the table 
 function autoScroll(){
-	const elmnt = document.querySelector("table pokeTable tr:last-child");
+	const elmnt = document.querySelector("pokemonDisplay tr:last-child");
   elmnt.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
 }
